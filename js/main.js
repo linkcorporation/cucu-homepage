29975c9aff7c1b8d379d0844f995b05f4fb20eee
Caress2250 <loveawanakulake0522@gmail.com>
2023-06-01 15:48:10 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -134,34 +134,63 @@ $(function () {
     element.remove();
     return result;
   }
-  window.onload = async function () {
-    // rss feed を取得
-    const res = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://cucu-81.tumblr.com/rss');
-    const data = await res.json();
-    // アイテムを１個ずつ表示
-    for (let i = 0; i < 3; i++){
-      const item = data.items[i];
-      console.log(item);
-      const thumbnail = getInstaImage(item.description);
-      const pubDate = item.pubDate;
-      const description = getSimpleText(item.description);
-      const instaUrl = getInstaLink(item.description);
-      const innerHTML = `
-      <div class="cucu-g-33 cucu-md-50 cucu-sm-100">
-        <a href="${instaUrl}" target="_blank" class="cucu-blog-card" data-no-swup="">
-          <div class="cucu-photo-frame cucu-active">
-            <img src="${thumbnail}" alt="photo">
-          </div>
-          <div class="cucu-post-text">
-            <div class="cucu-date">${pubDate}</div>
-            <p>${description}</p>
-          </div>
-        </a>
-      </div>
-      `;
-      document.getElementById('cucu-news-rss').innerHTML += innerHTML;
-    }
-  };
+  
+
+
+
+  const apiIndex = "https://2mmx7gzu7i.microcms.io/api/v1/blogs"; // APIのエンドポイント
+
+  // お知らせとして表示するブログ記事の数
+  const numBlogPosts = 3;
+  
+  // ブログデータを取得する関数
+  const getBlogData = () => {
+    // APIからデータを取得します
+    fetch(`${apiIndex}?limit=${numBlogPosts}`, {
+      headers: {
+        "X-API-KEY": "PEfp0AHyALckYsrJavaQSj0l1KOg6LhIUOww" // APIキー
+      }
+    })
+    .then(res => res.json())
+    .then(json => {
+      for (let i = 0; i < numBlogPosts; i++) {
+        // 取得したデータを元に表示を更新します
+        if (json.contents.length > i) {
+          let latestId = json.contents[i].id;
+          fetchSingleBlogData(latestId, i); // 各記事データを取得
+        } else {
+          document.getElementById(`blog-post-${i}`).style.display = "none"; // データがなければ該当の記事を非表示にします
+        }
+      }
+    })
+  }
+  
+  // 個々のブログデータを取得する関数
+  const fetchSingleBlogData = (latestId, index) => {
+    let apiUrl = `${apiIndex}/${latestId}`; // 最新のIDを使ってAPIのURLを組み立てる
+    // 個々の記事データをAPIから取得します
+    fetch(apiUrl, {
+      headers: {
+        "X-API-KEY": "PEfp0AHyALckYsrJavaQSj0l1KOg6LhIUOww" // APIキー
+      }
+    })
+    .then(res => res.json())
+    .then(json => {
+      // 取得したデータを使って表示を更新します
+      document.getElementById(`date${index}`).innerHTML = moment(json.publishedAt).format('YYYY年MM月DD日'); // 日付を表示
+      document.getElementById(`title${index}`).innerHTML = json.title; // タイトルを表示
+      let imgElement = document.getElementById(`eyecatch${index}`);
+      imgElement.src = json.eyecatch.url; // アイキャッチ画像を表示
+      imgElement.setAttribute("loading", "lazy"); // 遅延読み込み属性を追加
+  
+      // 記事へのリンクを設定
+      let blogPostElement = document.getElementById(`blog-post-${index}`);
+      blogPostElement.addEventListener('click', () => window.open('http://cucublog.com/', '_blank'));
+    })
+  }
+  
+  // ページの他の要素がすべてロードされた後にブログデータを読み込みます
+  window.onload = getBlogData;
 
 
   // =============================================x========
